bf376122762526545ce42f1473339b5fca18d07c
deny <d3nys.pavlov@gmail.com>
2024-11-26 16:28:50 +0200

    refactor: cron schedule

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import { updateRoomAvailability } from './controllers/booking.controller';
 const PORT = process.env.PORT || 5000;
 
 cron.schedule(
-  '0 */12 * * *',
+  '*/1 * * * *',
   async () => {
     console.log('Running room availability update...');
     await updateRoomAvailability();
