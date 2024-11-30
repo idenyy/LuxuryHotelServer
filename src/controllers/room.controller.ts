8d6ed17f1186f1ff2c9221ba61ea2b44805223e8
deny <d3nys.pavlov@gmail.com>
2024-11-30 17:34:04 +0200

    feat: added get available rooms endpoint

--- src/controllers/room.controller.ts
+++ src/controllers/room.controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express';
 import Room from '../models/room.model.js';
+import Booking from '../models/booking.model.js';
+import { Op } from 'sequelize';
 
 export const create = async (req: Request, res: Response): Promise<any> => {
   const { number, type, description, capacity } = req.body;
@@ -22,5 +24,40 @@ export const create = async (req: Request, res: Response): Promise<any> => {
   } catch (error) {
     console.error(error);
     return res.status(500).json({ message: 'Server error' });
+  }
+};
+
+export const getAvailable = async (req: Request, res: Response): Promise<any> => {
+  const { checkInDate, checkOutDate, capacity, type } = req.body;
+
+  if (!checkInDate || !checkOutDate || !capacity) return res.status(400).json({ error: 'Missing required fields: checkInDate, checkOutDate, or capacity' });
+  const checkIn = new Date(checkInDate);
+  const checkOut = new Date(checkOutDate);
+
+  try {
+    const rooms = await Room.findAll({
+      where: { capacity, type, isAvailable: true },
+      include: {
+        model: Booking,
+        as: 'bookings',
+        where: {
+          [Op.or]: [
+            { checkInDate: { [Op.between]: [checkIn, checkOut] } },
+            { checkOutDate: { [Op.between]: [checkIn, checkOut] } },
+            {
+              checkInDate: { [Op.lte]: checkIn },
+              checkOutDate: { [Op.gte]: checkOut }
+            }
+          ]
+        },
+        required: false
+      }
+    });
+
+    if (!rooms) return res.status(404).json({ error: 'Room Not Found or Not Available' });
+
+    return res.status(201).json(rooms);
+  } catch (error: any) {
+    console.error(error);
   }
 };
--- src/routes/room.route.ts
+++ src/routes/room.route.ts
@@ -1,11 +1,12 @@
 import express from 'express';
-import { create } from '../controllers/room.controller.js';
+import { create, getAvailable } from '../controllers/room.controller.js';
 import { getRoomReviews, createReview } from '../controllers/review.controller.js';
 import { authorization } from '../middleware/authorization.js';
 
 const router = express.Router();
 
 router.post('/', create);
+router.get('/', getAvailable);
 
 // Review
 router.post('/reviews', authorization, createReview);
